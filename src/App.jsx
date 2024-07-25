e46623dfe811c54732bb8fe524f5581177738e4e
Rohan Jaiswal <rj957977@gmail.com>
2024-07-25 22:31:26 +0530

    home page on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
+import Home from "./pages/home";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Home />
       <Footer />
     </div>
   );
