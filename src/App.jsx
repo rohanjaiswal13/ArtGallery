f35caa4156797408aa41051d68bd8e3ed5e62539
Rohan Jaiswal <rj957977@gmail.com>
2024-07-25 17:09:02 +0530

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import NavBar from "./components/navbar/navbar"
-
+import Footer from "./components/footer/footer"
 const App = () => {
 
   return (
       <div>
         <NavBar />
+        <Footer />
       </div>
   )
 }
