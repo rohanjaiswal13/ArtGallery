278dcccca3a259089265934ceac2d47aabb6f06c
Rohan Jaiswal <rj957977@gmail.com>
2024-07-26 12:59:38 +0530

    button

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,12 +5,24 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="">
           YourArt
         </a>
         <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2 search" type="search" placeholder="Search" aria-label="Search" />
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item mr">
