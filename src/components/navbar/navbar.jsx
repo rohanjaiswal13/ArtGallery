e04c87980e609daa2843a0f8b0846af75d1af76f
Rohan Jaiswal <rj957977@gmail.com>
2024-07-26 15:34:38 +0530

    search bar finalised

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -30,7 +30,7 @@ const NavBar = () => {
               aria-label="Search"
             />
             <button
-              className="btn btn-outline-success my-2 my-sm-0"
+              className="btn btn-outline-dark my-2 my-sm-0"
               type="submit"
             >
               Search
