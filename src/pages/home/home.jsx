99a4728669f4abdce946d1e7276b780b479bad1e
Rohan Jaiswal <rj957977@gmail.com>
2024-07-29 12:12:51 +0530

    Adding Carousel

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-
-function Home() {
+import car1 from "../../assets/images/car1.jpg"
+import car2 from "../../assets/images/car2.jpg"
+import car3 from "../../assets/images/car3.jpg"
+const Home = () => {
   return (
-    <>
-      <div>home page</div>
-    </>
+      <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={car1} className="d-block w-100"/>
+        </div>
+        <div className="carousel-item">
+          <img src={car2} className="d-block w-100" />
+        </div>
+        <div className="carousel-item">
+          <img src={car3} className="d-block w-100"/>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
   );
 }
 
