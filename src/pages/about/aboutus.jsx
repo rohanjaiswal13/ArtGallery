e77490bf587dc2b99ff86ce21ddf44fd1f86de98
Mohd Aftab Idrisi <aftab.idrisi0731@gmail.com>
2024-07-31 17:45:14 +0530

    pages

---
+++ src/pages/about/aboutus.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
+
+function AboutUs() {
+  return (
+    <>
+      <NavBar />
+      <div>About us Page</div>
+      <Footer />
+    </>
+  );
+}
+
+export default AboutUs;
---
+++ src/pages/blog/blog.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
+
+function Blog() {
+  return (
+    <>
+      <NavBar />
+      <div>Blog Page</div>
+      <Footer />
+    </>
+  );
+}
+
+export default Blog;
