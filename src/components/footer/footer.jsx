953f772f643612a208c726c4e697b8380d1a97a0
Mohd Aftab Idrisi <aftab.idrisi0731@gmail.com>
2024-08-03 12:25:38 +0530

    update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -30,73 +30,9 @@ const Footer = () => {
                 </Link>
               </li>
               <li className="nav-item mb-2">
-                <li className="nav-item">
-                  <Link className="nav-link mr" to="/aboutus">
-                    About
-                  </Link>
-                </li>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
+                <Link className="nav-link mr" to="/aboutus">
                   About
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  About
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
